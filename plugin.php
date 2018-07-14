3e096ee0c5aace1b07d4279f7a226278f78915b9
Azeem Mirza <contact@azeemirza.com>
2018-07-14 22:51:45 +0500

    Init

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Custom Post Type Projects
+ * Plugin URI: http://azeemirza.com
+ * Description: Manage projects
+ * Version: 0.0.0
+ * Author: Azeem Mirza
+ * Author URI: http://azeemirza.com
+ * Text Domain: custom-post-type-projects
+ * Domain Path: /languages/
+ * License: GPL2
+ * User: Azeem Mirza
+ * Date: 7/14/2018
+ * Time: 10:48 PM
+ * Created by PhpStorm.
+ */
+
