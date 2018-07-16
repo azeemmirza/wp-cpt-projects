30e2232cb8c683245568be590d52c2dcc817c78f
Azeem Mirza <contact@azeemirza.com>
2018-07-16 23:33:35 +0500

    Enqueuing on plugin index

--- plugin.php
+++ plugin.php
@@ -14,7 +14,29 @@
  * Time: 10:48 PM
  * Created by PhpStorm.
  */
+function scripts () {
+	wp_register_style(
+		'project-style',
+		plugin_dir_url(__DIR__ ).'projects/assets/style.css'
+	);
+	wp_enqueue_style('thickbox');
+	wp_enqueue_style( 'project-style' );
+
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('media-upload');
+	wp_enqueue_script('thickbox');
+
+	wp_register_script(
+		'project-script',
+		plugin_dir_url(__DIR__ ).'projects/assets/script.js',
+		array('jquery','media-upload','thickbox')
+		);
+	wp_enqueue_script('project-script');
+}
+
+add_action('admin_print_scripts', 'scripts');
 
 require( 'classes/Project.php' );
 if ( is_admin() )
+
 	require( 'classes/Project_Admin.php' );
