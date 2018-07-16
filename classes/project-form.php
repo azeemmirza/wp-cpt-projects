945bdbe52a52dd247a327fc1ae2ee0b93a6f3f12
Azeem Mirza <contact@azeemirza.com>
2018-07-16 23:33:02 +0500

    Form update

--- classes/project-form.php
+++ classes/project-form.php
@@ -9,15 +9,23 @@
 
 <div class="project-form">
 	<div class="buttons-wrapper">
-		<button type="button" id="add-image-meta" class="button">Add Text</button>
+
+		<button type="button" id="add-text-meta" class="button">Add Text</button>
 		<button type="button" id="add-image-meta" class="button">Add Image</button>
 	</div>
 
-	<div>
+	<div id="form-wrap" class="form-wrap">
+	</div>
+
+	<!--<div class="image-wrap">
 		<input id="upload_image" type="text" size="36" name="upload_image" value="" />
 		<input id="upload_image_button" type="button" value="Upload Image" class="button"/>
 	</div>
-
+	<div class="text-wrap">
+		<textarea></textarea>
+	</div>
+-->
+    <div class="image-block"> <img src="/" id="preview-img"><input id="upload_image" type="text" size="36" name="upload_image" value="" /><input id="upload_image_button" type="button" value="Upload Image" class="button"/></div>
 	<div class="submit-wrapper">
 		<?php submit_button(); ?>
 	</div>
